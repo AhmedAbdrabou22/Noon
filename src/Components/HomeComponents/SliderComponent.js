99d5a6c0df6e3d8073ec1ca55d0f738d95e4b772
AhmedAbdrabou22 <ahmedabdrabou723@gmail.com>
2023-09-13 20:01:31 +0300

    second edit

--- src/Components/HomeComponents/SliderComponent.js
+++ src/Components/HomeComponents/SliderComponent.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import SliderOne from '../../Images/slider1.avif'
 import SliderTwo from '../../Images/slider2.gif'
 import SliderThree from '../../Images/slider3.avif'
@@ -10,8 +11,12 @@ import SliderSeven from '../../Images/slider7.avif'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux"
 import { getAllCategory } from '../../redux/actions/GetAllCategoryAction';
+import Spinner from 'react-bootstrap/Spinner';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Slider = () => {
+const SliderCom = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getAllCategory());
@@ -21,57 +26,84 @@ const Slider = () => {
     if (categories) {
         console.log(categories);
     }
+
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 10,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: "linear",
+        centerMode: true,
+        centerPadding: "0px",
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                },
+                dots: true,
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
     return (
         <div className='shadow' style={{ paddingBottom: "15px", borderRadius: "10px" }}>
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={1}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
-                <SwiperSlide>
-                    <img src={SliderOne} alt="sliderImage" style={{ width: "100%" }} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={SliderTwo} alt="sliderImage" style={{ width: "100%" }} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={SliderThree} alt="sliderImage" style={{ width: "100%" }} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={SliderFour} alt="sliderImage" style={{ width: "100%" }} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={SliderFive} alt="sliderImage" style={{ width: "100%" }} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={SliderSix} alt="sliderImage" style={{ width: "100%" }} />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={SliderSeven} alt="sliderImage" style={{ width: "100%" }} />
-                </SwiperSlide>
-            </Swiper>
+            <Carousel activeIndex={index} onSelect={handleSelect} className="caursol">
+                <Carousel.Item>
+                    <img src={SliderOne} alt="data" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={SliderTwo} alt="data" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={SliderThree} alt="data" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={SliderFour} alt="data" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={SliderFive} alt="data" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={SliderSix} alt="data" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={SliderSeven} alt="data" />
+                </Carousel.Item>
+            </Carousel>
 
-            <Swiper
-                className='mt-4'
-                spaceBetween={5}
-                slidesPerView={12}
-                
-            >
+
+            <Slider {...settings} className='slider-item mt-3'>
                 {
-                    categories && categories.data ? (
-                        categories.data.map((item) => {
-                            return (
-                                <SwiperSlide className='slider'>
-                                    <img src={item.image} alt="categoryOne" />
-                                </SwiperSlide>
-                            )
+                    categories.data ? (
+                        categories.data.map((item, index) => {
+                            return (<div className='categoryItem'>
+
+                                <img src={item.image} alt="1" />
+                                <p>{item.title_ar}</p>
+
+                            </div>)
                         })
-                    ) : null
+                    ) : (<Spinner animation="grow" />)
                 }
-            </Swiper>
+            </Slider>
         </div>
     )
 }
 
-export default Slider
+export default SliderCom
